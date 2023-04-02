3e1c6116d18f3aae9be68029988f6e466384d715
tqer39 <tqer39@gmail.com>
2023-04-02 12:19:09 +0000

    tqer39リポジトリを作成

---
+++ terraform/src/repository/main.tf
@@ -0,0 +1,6 @@
+module "tqer39" {
+  source = "../../modules/repository"
+
+  branch     = "main"
+  repository = "tqer39"
+}
---
+++ terraform/src/repository/terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.61.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "5.0"
+    }
+  }
+  backend "s3" {
+    bucket  = "terraform-tfstate-tqer39-072693953877-ap-northeast-1"
+    encrypt = true
+    key     = "terraform-github/terraform/src/repository.tfstate"
+    region  = "ap-northeast-1"
+  }
+}
