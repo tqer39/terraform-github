df5a2aea3eae371fdd307a7cc3e2bbd9627efccd
tqer39 <tqer39@gmail.com>
2023-04-02 14:04:21 +0000

    remove

--- terraform/modules/repository/github_branch_default.tf
+++ terraform/modules/repository/github_branch_default.tf
@@ -1,6 +1,6 @@
 resource "github_branch_default" "this" {
   repository = github_repository.this.name
-  branch     = github_branch.this.branch
+  branch     = var.branch
 
   depends_on = [
     github_repository.this,
