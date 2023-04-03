e468a27869f5424f7c33803097353cb0939c3353
tqer39 <tqer39@gmail.com>
2023-04-03 08:17:44 +0000

    設定の省力化

--- terraform/src/repository/main.tf
+++ terraform/src/repository/main.tf
@@ -10,19 +10,7 @@ module "tqer39" {
     "main" = {
       require_pull_request_reviews    = true
       required_approving_review_count = 1
-      dismiss_stale_reviews           = false
-      require_code_owner_reviews      = false
-      restrict_dismissals             = false
-      dismissal_restrictions          = []
       required_status_checks          = true
-      status_check_up_to_date         = true
-      status_check_contexts           = []
-      require_signed_commits          = false
-      enforce_admins                  = false
-      push_restrictions               = []
-      allows_deletions                = false
-      allows_force_pushes             = false
-      required_linear_history         = false
     }
   }
 }
