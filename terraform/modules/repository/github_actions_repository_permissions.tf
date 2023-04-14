fd2bc9090b6283f37735962c286bb18facb58688
tqer39 <tqer39@gmail.com>
2023-04-14 23:00:16 +0000

    ♻️ allowed_actions = "selected"で動作するように修正

--- terraform/modules/repository/github_actions_repository_permissions.tf
+++ terraform/modules/repository/github_actions_repository_permissions.tf
@@ -2,10 +2,13 @@ resource "github_actions_repository_permissions" "this" {
   repository      = github_repository.this.name
   allowed_actions = var.allowed_actions
 
-  allowed_actions_config {
-    github_owned_allowed = var.github_owned_allowed
-    patterns_allowed     = var.patterns_allowed
-    verified_allowed     = var.verified_allowed
+  dynamic "allowed_actions_config" {
+    for_each = var.allowed_actions == "selected" ? [1] : []
+    content {
+      github_owned_allowed = var.github_owned_allowed
+      patterns_allowed     = var.patterns_allowed
+      verified_allowed     = var.verified_allowed
+    }
   }
 
   depends_on = [
