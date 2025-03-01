0b7af7f0b184f46d0806a313ec2fbb4f096b7e74
tqer39 <tqer39@gmail.com>
2025-03-01 15:36:14 +0900

    Fix quoting in GitHub secrets setting script for improved reliability

--- scripts/set-common-github-secrets.sh
+++ scripts/set-common-github-secrets.sh
@@ -9,8 +9,8 @@ repos=$(gh repo list "$1" --json name -q '.[].name')
 for repo in $repos; do
   echo "\nSetting secret for $repo"
 
-  gh secret set GHA_APP_ID --repo $1/$repo --body "$GHA_APP_ID"
-  gh secret set GHA_APP_PRIVATE_KEY --repo $1/$repo --body "$GHA_APP_PRIVATE_KEY"
-  gh secret set OPENAI_API_KEY --repo $1/$repo --body "$OPENAI_API_KEY"
+  gh secret set GHA_APP_ID --repo "$1/$repo" --body "$GHA_APP_ID"
+  gh secret set GHA_APP_PRIVATE_KEY --repo "$1/$repo" --body "$GHA_APP_PRIVATE_KEY"
+  gh secret set OPENAI_API_KEY --repo "$1/$repo" --body "$OPENAI_API_KEY"
 
 done
