e05f58d18d3ca7ccafb16ec0cada8f76ba560350
tqer39 <tqer39@gmail.com>
2025-03-22 10:45:57 +0900

    🔧 terraform_awsモジュールを新しいファイルに移動し、main.tfから削除

--- terraform/src/repository/main.tf
+++ terraform/src/repository/main.tf
@@ -1,19 +1,3 @@
-module "terraform_aws" {
-  source         = "../../modules/repository"
-  github_token   = var.github_token
-  repository     = "terraform-aws"
-  default_branch = "main"
-  topics         = ["terraform", "aws"]
-  description    = "Configure AWS resources with Terraform."
-  branches_to_protect = {
-    "main" = {
-      required_status_checks        = true
-      required_pull_request_reviews = true
-      status_check_contexts         = ["pre-commit", "terraform-aws-management", "terraform-aws-portfolio", "terraform-aws-sandbox"]
-    }
-  }
-}
-
 module "terraform_github" {
   source         = "../../modules/repository"
   github_token   = var.github_token
