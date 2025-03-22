07d3ac0acc0ec984f2b6d7362f90c5ea64a6523e
tqer39 <tqer39@gmail.com>
2025-03-22 10:49:04 +0900

    🔧 terraform_vercelモジュールを新しいファイルに移動し、main.tfから削除

---
+++ terraform/src/repository/terraform-vercel.tf
@@ -0,0 +1,14 @@
+module "terraform_vercel" {
+  source         = "../../modules/repository"
+  github_token   = var.github_token
+  repository     = "terraform-vercel"
+  default_branch = "main"
+  topics         = ["terraform", "vercel"]
+  description    = "Configure Vercel resources with Terraform."
+  branches_to_protect = {
+    "main" = {
+      required_status_checks        = true
+      required_pull_request_reviews = true
+    }
+  }
+}
