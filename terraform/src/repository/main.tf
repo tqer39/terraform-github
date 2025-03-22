632ad27554c8d56e821c6e9fbc515921dc5d85e1
tqer39 <tqer39@gmail.com>
2025-03-22 10:49:48 +0900

    🔧 openai_generate_pr_descriptionモジュールを新しいファイルに移動し、main.tfから削除

--- terraform/src/repository/main.tf
+++ terraform/src/repository/main.tf
@@ -29,19 +29,3 @@ module "time_capsule" {
     }
   }
 }
-
-module "openai_generate_pr_description" {
-  source         = "../../modules/repository"
-  github_token   = var.github_token
-  repository     = "openai-generate-pr-description"
-  default_branch = "main"
-  topics         = ["openai"]
-  description    = "Generate Pull Request description with OpenAI."
-  branches_to_protect = {
-    "main" = {
-      required_status_checks        = true
-      required_pull_request_reviews = true
-      status_check_contexts         = ["pre-commit"]
-    }
-  }
-}
