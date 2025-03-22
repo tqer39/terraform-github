25fceff11948491934e3a9a49ed983ae7a4d8327
tqer39 <tqer39@gmail.com>
2025-03-22 10:41:25 +0900

    🔧 Terraformモジュールの移動と設定の整理

--- terraform/src/repository/main.tf
+++ terraform/src/repository/main.tf
@@ -1,20 +1,3 @@
-module "tqer39" {
-  source         = "../../modules/repository"
-  github_token   = var.github_token
-  repository     = "tqer39"
-  default_branch = "main"
-  topics         = ["profile"]
-  description    = "personal information repository"
-
-  branches_to_protect = {
-    "main" = {
-      # GitHub Action のワークフローで main に push するために必要
-      allows_force_pushes           = true
-      required_pull_request_reviews = true
-    }
-  }
-}
-
 module "renovate_config" {
   source         = "../../modules/repository"
   github_token   = var.github_token
