83eb4b54e1b898c860d16457f69d22c593437f40
Takeru O'oyama <tqer39@gmail.com>
2025-05-28 00:49:51 +0900

    🚀 ローカルワークスペースプロビジョニングのTerraformモジュールの改善 (#1257)
    * ローカルワークスペースプロビジョニング用のTerraformモジュールを新規作成しました。リポジトリの設定や保護するブランチの要件を含んでいます。
    
    * ローカルワークスペースプロビジョニングのTerraformモジュールから空のトピック設定を削除しました。

---
+++ terraform/src/repository/local_workspace_provisioning.tf
@@ -0,0 +1,14 @@
+module "local_workspace_provisioning" {
+  source         = "../../modules/repository"
+  github_token   = var.github_token
+  repository     = "local-workspace-provisioning"
+  default_branch = "main"
+  description    = "Local workspace provisioning repository."
+  branches_to_protect = {
+    "main" = {
+      required_status_checks        = true
+      required_pull_request_reviews = true
+      status_check_contexts         = ["pre-commit"]
+    }
+  }
+}
