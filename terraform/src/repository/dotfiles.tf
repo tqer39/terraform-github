d2c8f135362dc1b877afba95586fc084a707df31
Takeru O'oyama <tqer39@gmail.com>
2025-05-30 00:17:41 +0900

    🔓 dotfilesリポジトリの可視性を「public」に変更 (#1258)
    * ✨ 新しいTerraformモジュール「dotfiles」を追加しました。リポジトリの設定や保護するブランチの要件を含んでいます。
    
    * 🔄 dotfilesリポジトリの可視性を「private」から「public」に変更しました。

---
+++ terraform/src/repository/dotfiles.tf
@@ -0,0 +1,16 @@
+module "dotfiles" {
+  source         = "../../modules/repository"
+  github_token   = var.github_token
+  repository     = "dotfiles"
+  default_branch = "main"
+  description    = "My dotfiles repository"
+  visibility     = "public"
+
+  branches_to_protect = {
+    "main" = {
+      required_status_checks        = true
+      required_pull_request_reviews = true
+      status_check_contexts         = []
+    }
+  }
+}
