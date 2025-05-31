b523286d0ffb2b47d52fe7cde8e8e0419c1538ad
tqer39 <tqer39@gmail.com>
2025-05-31 09:31:06 +0900

    ✨ 新しいTerraformモジュール「update-license-year」を追加しました。リポジトリのライセンス年を自動的に更新する機能を提供し、保護するブランチの要件を設定しています。

---
+++ terraform/src/repository/update-license-year.tf
@@ -0,0 +1,15 @@
+module "update-license-year" {
+  source         = "../../modules/repository"
+  github_token   = var.github_token
+  repository     = "update-license-year"
+  default_branch = "main"
+  topics         = ["license"]
+  description    = "Automatically update license year in repositories."
+  branches_to_protect = {
+    "main" = {
+      required_status_checks        = true
+      required_pull_request_reviews = true
+      status_check_contexts         = ["pre-commit"]
+    }
+  }
+}
