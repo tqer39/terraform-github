81540616f805072bb8b4c20986fbfe6d7f19f91b
Takeru O'oyama <tqer39@gmail.com>
2025-06-21 09:53:57 +0900

    feat: add conditional flag for actions permissions configuration

--- terraform/modules/repository/github_actions_repository_permissions.tf
+++ terraform/modules/repository/github_actions_repository_permissions.tf
@@ -1,15 +1,11 @@
 resource "github_actions_repository_permissions" "this" {
-  repository      = github_repository.this.name
-  allowed_actions = var.allowed_actions
-
-  dynamic "allowed_actions_config" {
-    for_each = var.allowed_actions == "selected" ? [1] : []
-    content {
-      github_owned_allowed = var.github_owned_allowed
-      patterns_allowed     = var.patterns_allowed
-      verified_allowed     = var.verified_allowed
-    }
-  }
+  count = var.configure_actions_permissions ? 1 : 0
+  
+  repository       = github_repository.this.name
+  allowed_actions  = var.allowed_actions
+  github_owned_allowed = var.github_owned_allowed
+  patterns_allowed = var.patterns_allowed
+  verified_allowed = var.verified_allowed
 
   depends_on = [
     github_repository.this,
