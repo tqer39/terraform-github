ca4b7b409982a6982bc27967abd0021437a9ab3a
Takeru O'oyama <tqer39@gmail.com>
2025-07-01 21:52:58 +0900

    ✨ 不要な「visibility」フィールドの削除とリポジトリ設定の更新 (#1300)
    * ✨ 新しい通知リポジトリモジュールの追加
    
    * 🔧 リポジトリの可視性を「private」から「public」に変更
    
    * 🗑️ 不要な「visibility」フィールドを削除

---
+++ terraform/src/repository/notifications.tf
@@ -0,0 +1,38 @@
+module "notifications" {
+  source                 = "../../modules/repository"
+  github_token           = var.github_token
+  repository             = "notifications"
+  owner                  = "tqer39"
+  default_branch         = "main"
+  enable_owner_bypass    = true
+  topics                 = ["notifications", "api"]
+  description            = "A repository for managing notification services."
+  delete_branch_on_merge = true
+  branch_rulesets = {
+    "main" = {
+      enforcement = "active"
+      conditions = {
+        ref_name = {
+          include = ["~DEFAULT_BRANCH"]
+          exclude = []
+        }
+      }
+      rules = {
+        pull_request = {
+          dismiss_stale_reviews_on_push     = true
+          require_code_owner_review         = false
+          required_approving_review_count   = 1
+          required_review_thread_resolution = true
+        }
+        required_status_checks = {
+          required_check = [
+            {
+              context = "pre-commit"
+            }
+          ]
+          strict_required_status_checks_policy = true
+        }
+      }
+    }
+  }
+}
