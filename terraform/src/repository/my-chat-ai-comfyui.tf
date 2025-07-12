70a9aef1726bdd813b7331d3193ac1b8297df926
Takeru O'oyama <tqer39@gmail.com>
2025-07-12 10:56:27 +0900

    ✨ cspell設定のインライン化とTerraformファイルの追加 (#1318)
    * Remove unnecessary moved block and update cspell
    
    * chore: inline cspell words

---
+++ terraform/src/repository/my-chat-ai-comfyui.tf
@@ -0,0 +1,29 @@
+module "my-chat-ai-comfyui" {
+  source              = "../../modules/repository"
+  github_token        = var.github_token
+  repository          = "my-chat-ai-comfyui"
+  owner               = "tqer39"
+  default_branch      = "main"
+  enable_owner_bypass = true
+  topics              = ["comfyui", "chat-ai"]
+  description         = "Integrate chat AI features with ComfyUI"
+  branch_rulesets = {
+    "main" = {
+      enforcement = "active"
+      conditions = {
+        ref_name = {
+          include = ["~DEFAULT_BRANCH"]
+          exclude = []
+        }
+      }
+      rules = {
+        pull_request = {
+          dismiss_stale_reviews_on_push     = true
+          require_code_owner_review         = false
+          required_approving_review_count   = 1
+          required_review_thread_resolution = true
+        }
+      }
+    }
+  }
+}
