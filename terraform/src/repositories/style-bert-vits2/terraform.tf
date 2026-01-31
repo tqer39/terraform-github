56de4d004f14517550988b0993dbfbb1b4ae07bd
Takeru O'oyama <tqer39@gmail.com>
2026-01-31 16:18:29 +0900

    🚀 Style-Bert-VITS2リポジトリのTerraform設定を追加 (#1470)
    * feat: add Style-Bert-VITS2 repository configuration with GitHub provider and branch rulesets
    
    * feat: add .terraform.lock.hcl for Style-Bert-VITS2 repository configuration

---
+++ terraform/src/repositories/style-bert-vits2/terraform.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "1.14.4"
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "6.10.2"
+    }
+  }
+  backend "s3" {
+    bucket  = "terraform-tfstate-tqer39-072693953877-ap-northeast-1"
+    key     = "terraform-github/repositories/style-bert-vits2.tfstate"
+    region  = "ap-northeast-1"
+    encrypt = true
+  }
+}
