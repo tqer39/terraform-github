24fdbc959279f8ca424a168e4df2a4bf39a9f97b
Takeru O'oyama <tqer39@gmail.com>
2026-04-06 23:29:54 +0900

    🚀 OPENAI_API_KEYをOPENROUTER_API_KEYに変更し、ワークフローを更新 (#1538)
    * 🚀 generate-pr-description.ymlの更新: APIキーとモデルの変更、バージョンをv2.0.1にアップデート
    
    * OPENAI_API_KEYをOPENROUTER_API_KEYに変更し、シークレット設定スクリプトを更新

--- scripts/set-common-github-secrets.sh
+++ scripts/set-common-github-secrets.sh
@@ -19,5 +19,5 @@ for repo in $repos; do
 
   gh secret set GHA_APP_ID --repo "$1/$repo" --body "$GHA_APP_ID"
   gh secret set GHA_APP_PRIVATE_KEY --repo "$1/$repo" --body "$GHA_APP_PRIVATE_KEY"
-  gh secret set OPENAI_API_KEY --repo "$1/$repo" --body "$OPENAI_API_KEY"
+  gh secret set OPENROUTER_API_KEY --repo "$1/$repo" --body "$OPENROUTER_API_KEY"
 done
